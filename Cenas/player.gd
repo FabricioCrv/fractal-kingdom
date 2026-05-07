29a7d815c39a34d8b5fc3020f2f0905279994d41
FabricioCrv <fabriciogarciacarvalho87@hotmail.com>
2026-05-07 09:05:44 -0300

    adicionando mecanica de empurrar os blocos

--- Cenas/player.gd
+++ Cenas/player.gd
@@ -1,37 +1,61 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var interaction_timer: Timer = $InteractionTimer
 
 @export var speed: float = 200.0
 var screen_size: Vector2
 var posicao_inicial: Vector2 = Vector2(640,690)
-
+var lastDirection: Vector2
+var currentCollider: Node2D
 
 func _ready():
 	screen_size = get_viewport_rect().size
 
-func _process(delta):
-	var velocity = Vector2.ZERO
-	
-	if Input.is_action_pressed("up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("down"):
-		velocity.y += 1
-	if Input.is_action_pressed("left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("right"):
-		velocity.x += 1
-	
-	position += velocity * speed * delta
-	position.y = clamp(position.y, 0.0, screen_size.y)
-	
-	if velocity.y > 0:
-		$AnimatedSprite2D.play("player_move_down")
-	elif velocity.y < 0:
-		$AnimatedSprite2D.play("player_move_up")
-	elif velocity.x < 0:
-		$AnimatedSprite2D.play("player_move_left")
-	elif velocity.x > 0:
-		$AnimatedSprite2D.play("player_move_right")
+func _physics_process(_delta: float) -> void:
+	var direction := Input.get_vector("left","right","up","down")
+	if direction:
+		velocity = direction * speed
 	else:
-		$AnimatedSprite2D.stop()
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.y = move_toward(velocity.y, 0, speed)
 	
+	var isColliding = move_and_slide()
+	
+	if isColliding and direction:
+		if interaction_timer.is_stopped():
+			interaction_timer.start()
+		currentCollider = get_last_slide_collision().get_collider()
+	else:
+		interaction_timer.stop()
+	
+	_set_animation(direction)
+	
+	lastDirection = direction
+		
+		
+	
+
+func _set_animation(direction):
+		
+	if direction:
+		if direction.y > 0:
+			animated_sprite_2d.play("player_move_down")
+		elif direction.y < 0:
+			animated_sprite_2d.play("player_move_up")
+		elif direction.x > 0:
+			animated_sprite_2d.play("player_move_right")
+		elif direction.x < 0:
+			animated_sprite_2d.play("player_move_left")
+		else:
+			if lastDirection.y > 0:
+				animated_sprite_2d.play("player_idle")
+			elif lastDirection.y < 0:
+				animated_sprite_2d.play("player_idle")
+			elif lastDirection.x != 0:
+				animated_sprite_2d.play("player_idle")
+
+
+func _on_interaction_timer_timeout() -> void:
+	if currentCollider is BlocoNumero:
+		currentCollider.push_block(lastDirection)
